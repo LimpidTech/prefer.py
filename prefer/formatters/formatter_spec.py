d09e519fb7a9d9c7aafec82fa09cf654bf47d776
Bailey Stoner <monokrome@monokro.me>
2017-09-18 11:43:10 -0700

    Repair formatter tests

--- prefer/formatters/formatter_spec.py
+++ prefer/formatters/formatter_spec.py
@@ -4,34 +4,37 @@ import pytest
 from prefer.formatters import formatter
 
 
-def raises_expected_exception(fn, *, kind: Exception=Exception):
-    raised = False
-
-    try:
-        result = fn()
-
-        if isinstance(result, types.GeneratorType):
-            yield from result
-
-    except kind:
-        raised = True
-
-    return raised
-
-
 @pytest.mark.asyncio
 async def test_Formatter_provide_raises_NotImplementedError():
-    test_fn = functools.partial(formatter.Formatter.provides, None)
-    assert raises_expected_exception(test_fn, kind=NotImplementedError)
+    caught_exception = False
+
+    try:
+        formatter.Formatter.provides(None)
+    except NotImplementedError:
+        caught_exception = True
+
+    assert caught_exception
 
 
 @pytest.mark.asyncio
 async def test_Formatter_serialize_raises_NotImplementedError():
-    test_fn = functools.partial(formatter.Formatter().serialize, {})
-    assert raises_expected_exception(test_fn, kind=NotImplementedError)
+    caught_exception = False
+
+    try:
+        await formatter.Formatter().serialize({})
+    except NotImplementedError:
+        caught_exception = True
+
+    assert caught_exception
 
 
 @pytest.mark.asyncio
 async def test_Formatter_deserialize_raises_NotImplementedError():
-    test_fn = functools.partial(formatter.Formatter().deserialize, '{}')
-    assert raises_expected_exception(test_fn, kind=NotImplementedError)
+    caught_exception = False
+
+    try:
+        await formatter.Formatter().deserialize('{}')
+    except NotImplementedError:
+        caught_exception = True
+
+    assert caught_exception
